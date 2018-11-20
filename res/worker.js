ec51d678ef4a4c5ef95dfc59fd9de849a63fe19c
Guillaume Martigny <guillaume.martigny@wiztivi.com>
2018-11-20 16:52:00 +0100

    Progress is register every 100ms and array results are split.

--- res/worker.js
+++ res/worker.js
@@ -1,16 +1,18 @@
 this.importScripts("./worker-protocol.js");
 this.importScripts("../script.js");
 
-let progressCall = 0;
+const progressDelay = 100;
+let lastProgress = -progressDelay;
 /**
  * Register script progress
  * @param {Number} ratio - Progress between 0 and 1
  * @param {Boolean} [forced=false] - Force the progression send
  */
 function progression (ratio, forced) {
-    if (++progressCall % 1e5 === 0 || forced) {
+    const now = performance.now();
+    if (now - lastProgress > 100 || forced) {
         this.postMessage(new Message(Message.types.progress, ratio));
-        progressCall = 0;
+        lastProgress = now;
     }
 }
 /**
@@ -18,7 +20,12 @@ function progression (ratio, forced) {
  * @param {String} message - Any message
  */
 function send (message) {
-    this.postMessage(new Message(Message.types.output, message));
+    if (Array.isArray(message)) {
+        message.forEach(m => send(m));
+    }
+    else {
+        this.postMessage(new Message(Message.types.output, message));
+    }
 }
 
 this.onmessage = (message) => {
